301ede31c8fd7668b3b9293daaa2df9bb029ec5a
michalkandybowicz <eejdzent@gmail.com>
2019-05-30 09:40:27 +0200

    taki dodaje się po wpisaniu oddzielajc je przecinkiem, gdyby by to enter wysyo by formularz xd
    Signed-off-by: michalkandybowicz <eejdzent@gmail.com>

--- templates/tags.js
+++ templates/tags.js
@@ -1,27 +1,35 @@
 [].forEach.call(document.getElementsByClassName('tags-input'), function (el) {
     let hiddenInput = document.createElement('input'),
-        mainInput = document.createElement('input');
+        mainInput = document.createElement('input'),
+        tags = [];
 
     hiddenInput.setAttribute('type', 'hidden');
-    hiddenInput.setAttribute('type', el.getAttribute('data-name'));
+    hiddenInput.setAttribute('name', el.getAttribute('data-name'));
 
     mainInput.setAttribute('type', 'text');
-    mainInput.setAttribute.add('main-input');
+    mainInput.classList.add('main-input');
     mainInput.addEventListener('input', function () {
-       let enteredTags = mainInput.value.split('.');
-       if (enteredTags.length > 1) {
-           enteredTags.forEach(function (t) {
-               let filteredTag = filterTag(t);
-               if (filteredTag.length > 0)
-                   addTag(filteredTag)
-           })
-       }
+        let enteredTags = mainInput.value.split(',');
+        if (enteredTags.length > 1) {
+            enteredTags.forEach(function (t) {
+                let filteredTag = filterTag(t);
+                if (filteredTag.length > 0)
+                    addTag(filteredTag);
+            });
+            mainInput.value = '';
+        }
+    });
+
+    mainInput.addEventListener('keydown', function (e) {
+        let keyCode = e.which || e.keyCode;
+        if (keyCode === 8 && mainInput.value.length === 0 && tags.length > 0) {
+            removeTag(tags.length - 1);
+        }
     });
 
     el.appendChild(mainInput);
     el.appendChild(hiddenInput);
 
-    // addTag('hello world');  //do testowania
 
     function addTag(text) {
         let tag = {
@@ -33,26 +41,24 @@
         tag.element.textContent = tag.text;
 
         let closeBtn = document.createElement('span');
-        closeBts.classList.add('close');
+        closeBtn.classList.add('close');
         closeBtn.addEventListener('click', function () {
-            removeTag(tags.indexOf(tag))
-        })
-
+            removeTag(tags.indexOf(tag));
+        });
         tag.element.appendChild(closeBtn);
 
-        tags.push(tag)
+        tags.push(tag);
 
         el.insertBefore(tag.element, mainInput);
 
-        refreshTags()
-
+        refreshTags();
     }
 
     function removeTag(index) {
         let tag = tags[index];
         tags.splice(index, 1);
         el.removeChild(tag.element);
-        refreshTags()
+        refreshTags();
     }
 
     function refreshTags() {
@@ -60,12 +66,10 @@
         tags.forEach(function (t) {
             tagsList.push(t.text);
         });
-        hiddenInput.value = tagsList.join('.');
+        hiddenInput.value = tagsList.join(',');
     }
 
     function filterTag(tag) {
-        return tag.replace(/[^\w -]/g, '').trim().replace(/\w+/g, '-');
+        return tag.replace(/[^\w -]/g, '').trim().replace(/\W+/g, '-');
     }
-
 });
-
